32a24195a202de6916f8f9daf4de41d099950763
Toi Koizumi <toui_16n@nnn.ed.jp>
2017-07-12 16:08:52 +0900

    リスト4-48

---
+++ tag_tagged.js
@@ -0,0 +1,24 @@
+// 与えられた文字列をエスケープ処理
+function escapeHtml (str) {
+    if (!str) { return ''; }
+    str = str.replace(/&/g, '&map;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '$#39;');
+    return str;
+}
+
+// 分解されたtemplatesとvaluesを順に連結(valuesはescapeHtml関数でエスケープ)
+function e(templates, ...values) {
+    let result = '';
+    for (let i = 0, len = templates.length; i < len; i++) {
+        result += templates[i] + escapeHtml(values[i]);
+    }
+    return result;
+}
+
+// テンプレート文字列をエスケープ処理
+let name = '<"Mario" & \'Luigi\'>';
+console.log(e`こんにちは、${name}さん！`);
+// 結果 : こんにちは、$lt;&quot;Mario&quot; &amp; &#39;Luigi&#39;&gt;さん！
